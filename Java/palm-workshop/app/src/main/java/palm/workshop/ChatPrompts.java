0d5a89ab2fa75d8f85d83b8b6df49216f2de947c
Rais Kazi <meetrais@gmail.com>
2024-04-17 06:54:57 -0500

    Code for PromptEngineering.

--- Java/palm-workshop/app/src/main/java/palm/workshop/ChatPrompts.java
+++ Java/palm-workshop/app/src/main/java/palm/workshop/ChatPrompts.java
@@ -5,12 +5,16 @@ import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
 import dev.langchain4j.chain.ConversationalChain;
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.UserMessage;
 
 public class ChatPrompts {
     public static void main(String[] args){
         //SimpleChat();
 
-        PromptAndChat();
+        //PromptAndChat();
+
+        PromptEngineering();
     }
 
     private static void SimpleChat()
@@ -87,4 +91,70 @@ public class ChatPrompts {
         
 
     }
+
+    static class Person {
+        String name;
+        int age;
+        String education;
+        String interest;
+        String achievement;
+    }
+
+    interface PersonExtractor {
+        @UserMessage("""
+            Extract the name and age of the person described below.
+            Return a JSON document with a "name", "age", "education", 
+            "interest", "achievement" properties, \
+            following this structure: {"name": "John Doe", "age": 34,
+            "education":"Computer Science", "interest":"reading books",
+            "achievement":"award in the field"}
+            Return only JSON, without any markdown markup surrounding it.
+            Here is the document describing the person:
+            ---
+            {{it}}
+            ---
+            JSON: 
+            """)
+            Person extractPerson(String text);
+    }
+
+    private static void PromptEngineering()
+    {
+        VertexAiChatModel model = VertexAiChatModel.builder()
+        .endpoint("us-central1-aiplatform.googleapis.com:443")
+        .project("palm-workshop-project")
+        .location("us-central1")
+        .publisher("google")
+        .modelName("chat-bison@001")
+        .maxOutputTokens(300)
+        .maxRetries(3)
+        .build();
+
+        PersonExtractor extractor = AiServices.create(PersonExtractor.class, model);
+
+        Person person = extractor.extractPerson("""
+            Anna is a 23 year old artist based in Brooklyn, New York. She was born and 
+            raised in the suburbs of Chicago, where she developed a love for art at a 
+            young age. She attended the School of the Art Institute of Chicago, where 
+            she studied painting and drawing. After graduating, she moved to New York 
+            City to pursue her art career. Anna's work is inspired by her personal 
+            experiences and observations of the world around her. She often uses bright 
+            colors and bold lines to create vibrant and energetic paintings. Her work 
+            has been exhibited in galleries and museums in New York City and Chicago.    
+            """
+        );
+
+        System.out.println("Name: " + person.name);
+        System.out.println("Age: " + person.age);
+        System.out.println("Education:" + person.education);
+        System.out.println("Interest: " + person.interest);
+        System.out.println("Achievement: " + person.achievement);
+
+
+
+
+    }
+
+
 }
+
